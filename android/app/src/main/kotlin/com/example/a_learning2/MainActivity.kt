e047b94de65e508ed03c39518eef8ba38ea3c203
AlulCode45 <alulgans70@gmail.com>
2024-12-31 08:22:24 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/a_learning2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a_learning2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:a_learning2/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+
+      ),
+    );
+  }
+}
